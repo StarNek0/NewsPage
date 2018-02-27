1a70a2216c73e462522a8ff606d09e19caa8bf0c
zsdostar <zsdzzydos@gmail.com>
2018-02-27 23:26:34 +0800

    完成对项目申报文件的爬取

--- Tools/NewsSpider/sites/policy_of_gaoxin.py
+++ Tools/NewsSpider/sites/policy_of_gaoxin.py
@@ -28,7 +28,7 @@ def get_urls(in_url):
     temp_text = root.xpath('//div[@class="text-center pagin mt20 pb20"]/div/text()')[0]
     index_num = int(re.search(u'/(\d+)页', temp_text).group(1))
     # 从每页获取每个标题的URL并存入urls中
-    for i in range(1,index_num+1):
+    for i in range(1, index_num + 1):
         thispage_url = in_url.replace('/index.jhtml', '/index_%d.jhtml' % i)
         content = requests.get(thispage_url).content
         root = etree.HTML(content)
@@ -39,7 +39,10 @@ def get_urls(in_url):
 
 
 def get_each_gov_msg(every_page_url):
+    db = pymysql.connect(host="localhost", user="root", password="klxsxzsdf1", db="newsspider", port=3306,
+                         charset='utf8')
     for each_page_url in every_page_url:
+        cid = re.search('(\d+)\.jhtml', each_page_url).groups(1)[0]
         content = requests.get(each_page_url).content
         root = etree.HTML(content)
 
@@ -51,26 +54,84 @@ def get_each_gov_msg(every_page_url):
         total_content = root.xpath('string(//div[@id="d_content"])')  # ！！！！！！！！！！！！！！！
         content = root.xpath('//div[@id="d_content"]/p/text()')
         end_right_content_num = len(root.xpath('//div[@id="d_content"]/p[@style="text-align: right;"]/text()'))
-        main_content, end_content = content[0:end_right_content_num*(-1)], content[end_right_content_num*(-1):]
+        main_content, end_content = '\n'.join(content[0:end_right_content_num * (-1)]), '\n'.join(content[end_right_content_num * (-1):])
+
+        # print title, date, re.search(u'来源:(.+)',from_gov).groups(1)[0]
+        # print total_content.strip()
+        # print main_content, end_content
+
+        # 数据库查重
+        cur = db.cursor()
+        sql = "select count(*) from news_gov_msg where url='%s'" % each_page_url
+        cur.execute(sql)
+        res = cur.fetchone()[0]
+        if res:
+            print title, 'has existed in the database.'
+            cur.close()
+        else:
+            # 入库爬到的数据
+            sql = "INSERT INTO news_gov_msg(title, url, msg_from, msg_date, whole_content, main_content, end_content) VALUES('%s','%s','%s','%s','%s','%s','%s')" % (
+            title, each_page_url, re.search(u'来源:(.+)',from_gov).groups(1)[0], date, total_content.strip(), main_content, end_content)
+            cur.execute(sql)
+            db.commit()
+            cur.close()
 
         download_url_num = len(root.xpath('//*[starts-with(@id, "attach")]'))  # ！！！！！！！！！！！
         download_url_name = root.xpath('//*[starts-with(@id, "attach")]/text()')
 
-        # print title, date, from_gov
-        # print total_content
-        from selenium import webdriver
+        down_url = 'http://www.cdht.gov.cn/attachment_url.jspx?cid=%s&n=%d' %(cid, download_url_num)
+        json_key = requests.get(down_url).json()
+        for i in range(download_url_num):
+            real_url = 'http://www.cdht.gov.cn/attachment.jspx?cid=%s&i=%d' % (cid, i) + json_key[i]
+            # print real_url
 
+            # 查重
+            cur = db.cursor()
+            sql = "select count(*) from news_file where file_name='%s' and from_url='%s'" % (download_url_name[i], each_page_url)
+            cur.execute(sql)
+            res = cur.fetchone()[0]
+            if res:
+                print download_url_name[i], 'has existed in the database.'
+                cur.close()
+                continue
 
-        browser = webdriver.Chrome(executable_path='chromedriver.exe')
-        browser.get(each_page_url)
-        time.sleep(2)
-        download_url = browser.find_element_by_id('attach0').get_attribute('href')
-        print download_url_name, download_url
-        browser.quit()
-
+            # 插文件信息入库
+            sql = "INSERT INTO news_file(from_url, file_name, file_url) VALUES ('%s','%s','%s')" % (
+            each_page_url, download_url_name[i], real_url)
+            cur.execute(sql)
+            db.commit()
+            cur.close()
+        # 通过selenium爬动态内容
+        # from selenium import webdriver
+        #
+        # try:
+        #     browser = webdriver.Chrome(executable_path='chromedriver.exe')
+        #     browser.get(each_page_url)
+        #     for i in range(download_url_num):
+        #         download_url = browser.find_element_by_id('attach%d' % i).get_attribute('href')
+        #         # print download_url_name[i], download_url
+        #         cur = db.cursor()
+        #
+        #         # 查重
+        #         sql = "select count(*) from news_file where file_url='%s'" % download_url
+        #         cur.execute(sql)
+        #         res = cur.fetchone()[0]
+        #         if res:
+        #             print download_url, 'has existed in the database.'
+        #             cur.close()
+        #             continue
+        #
+        #         # 插文件信息入库
+        #         sql = "INSERT INTO news_file(from_url, file_name, file_url) VALUES ('%s','%s','%s')" % (each_page_url, download_url_name[i], download_url)
+        #         cur.execute(sql)
+        #         db.commit()
+        #         cur.close()
+        # finally:
+        #     browser.quit()
+    db.close()
 
 if __name__ == '__main__':
     url = u'http://www.cdht.gov.cn/jbxxgk/index.jhtml?title=%E7%94%B3%E6%8A%A5'
-    # every_page_url = get_urls(url)
-    every_page_url = ['http://www.cdht.gov.cn/zwgktzgg/143370.jhtml']
+    every_page_url = get_urls(url)
+    # every_page_url = ['http://www.cdht.gov.cn/zwgktzgg/139755.jhtml']
     get_each_gov_msg(every_page_url)
--- news/models.py
+++ news/models.py
@@ -16,7 +16,32 @@ class news(models.Model):
     keywords = models.CharField(max_length=255, verbose_name="关键词")
     page_date = models.CharField(max_length=255, verbose_name="日期")
     class Meta:
-        verbose_name='theme'
-        verbose_name_plural='theme'
+        verbose_name='news'
+        verbose_name_plural='news'
     def __unicode__(self):
         return self.theme
+
+class GOV_MSG(models.Model):
+    title = models.CharField(max_length=255, verbose_name="标题")
+    url = models.CharField(max_length=255, verbose_name="URL")
+    msg_from = models.CharField(max_length=255, verbose_name="来源")
+    msg_date = models.CharField(max_length=255, verbose_name="发布日期")
+    whole_content = models.TextField(verbose_name="全文", null=True, blank=True)
+    main_content = models.TextField(verbose_name="正文", null=True, blank=True)
+    end_content = models.TextField(verbose_name="落款", null=True, blank=True)
+    class Meta:
+        verbose_name='gov'
+        verbose_name_plural='gov'
+    def __unicode__(self):
+        return self.title
+
+
+class FILE(models.Model):
+    from_url = models.CharField(max_length=255, verbose_name="URL")
+    file_name = models.CharField(max_length=255, verbose_name="标题")
+    file_url = models.CharField(max_length=255, verbose_name="标题")
+    class Meta:
+        verbose_name='file'
+        verbose_name_plural='file'
+    def __unicode__(self):
+        return self.file_name
