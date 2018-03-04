1f93594774f220b79659824ca896c1fafdd7425e
zsdostar <zsdzzydos@gmail.com>
2018-03-04 15:44:02 +0800

    正序倒序筛选时间等

---
+++ Tools/NewsSpider/sites/policy_update.py
@@ -0,0 +1,26 @@
+# coding:utf8
+"""
+--------------------------------------------------------------------------
+    File:   policy_update.py
+    Auth:   zsdostar
+    Date:   2018/3/4 14:37
+    Sys:    Windows 10
+--------------------------------------------------------------------------
+    Desc:   
+--------------------------------------------------------------------------
+"""
+import pymysql
+import time
+db = pymysql.connect(host="localhost", user="root", password="klxsxzsdf1", db="newsspider", port=3306,
+                     charset='utf8')
+cur = db.cursor()
+sql = "select id,msg_date,float_date from news_gov_msg;"
+cur.execute(sql)
+res = cur.fetchall()
+for x in res:
+    if x[2] is None:
+        float_date = time.mktime(time.strptime(x[1], "%Y-%m-%d"))
+        cur.execute("update news_gov_msg set float_date=%f where id=%d" % (float_date,x[0]))
+        db.commit()
+cur.close()
+db.close()
--- news/views.py
+++ news/views.py
@@ -1,10 +1,10 @@
-#coding:utf-8
+# coding:utf-8
 import time
 from django.shortcuts import render
 from django.views.generic.base import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
-from .models import news,GOV_MSG,FILE
+from .models import news, GOV_MSG, FILE
 
 
 def DtCalc(stTime, edTime):
@@ -13,15 +13,16 @@ def DtCalc(stTime, edTime):
 
     return int((ed - st) / (24 * 3600))  # 时间差/天
 
+
 class NewsView(View):
     def get(self, request, date=None):
         if date is None:
             date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
         DATE_Array = time.strptime(date, '%Y-%m-%d')
         DATE_Stamp = int(time.mktime(DATE_Array))
-        front_date=time.strftime('%Y-%m-%d', time.localtime(DATE_Stamp-24*3600))
-        behind_date=time.strftime('%Y-%m-%d', time.localtime(DATE_Stamp+24*3600))
-        
+        front_date = time.strftime('%Y-%m-%d', time.localtime(DATE_Stamp - 24 * 3600))
+        behind_date = time.strftime('%Y-%m-%d', time.localtime(DATE_Stamp + 24 * 3600))
+
         News_pages = news.objects.filter(page_date=date, tag=u'资讯').order_by('-score')
         Policy_pages = news.objects.filter(page_date=date, tag=u'政策').order_by('-score')
         Company_pages = news.objects.filter(page_date=date, tag=u'企业').order_by('-score')
@@ -29,27 +30,27 @@ class NewsView(View):
         YingYong_pages = news.objects.filter(page_date=date, tag=u'应用').order_by('-score')
 
         return render(request, 'index.html', {
-            'News_pages':News_pages,
-            'Policy_pages':Policy_pages,
-            'Company_pages':Company_pages,
-            'ChanYe_pages':ChanYe_pages,
-            'YingYong_pages':YingYong_pages,
+            'News_pages': News_pages,
+            'Policy_pages': Policy_pages,
+            'Company_pages': Company_pages,
+            'ChanYe_pages': ChanYe_pages,
+            'YingYong_pages': YingYong_pages,
 
             'date': date,
             'format_date': date[5:],
 
-            'front_date':front_date,
-            'behind_date':behind_date,
+            'front_date': front_date,
+            'behind_date': behind_date,
         })
 
 
 class ProjectView(View):
 
     def get(self, request):
-        all_projects = GOV_MSG.objects.all()
+        recent = time.time() - 64 * 24 * 3600  # 只要两个月内的信息
+        recent_projects = GOV_MSG.objects.filter(float_date__gt=recent).order_by('-id')
 
         # hot_orgs = all_orgs.order_by('-click_num')[:3]
-
 
         # 搜索功能
         # search_keywords = request.GET.get('keywords', '')
@@ -65,8 +66,25 @@ class ProjectView(View):
         # 机构类别筛选
         category = request.GET.get('ct', '')
         if category:
-            all_projects = all_projects.filter(site_from=category)
+            recent_projects = recent_projects.filter(site_from=category)
 
+        time_recent = request.GET.get('timerecent', '')
+        if time_recent:
+            if time_recent == 'threeday':
+                recent = time.time() - 3 * 24 * 3600  # 三天内的信息
+            elif time_recent == 'week':
+                recent = time.time() - 7 * 24 * 3600  # 一周内的信息
+            elif time_recent == 'month':
+                recent = time.time() - 31 * 24 * 3600  # 一个月内的信息
+
+            recent_projects = recent_projects.filter(float_date__gt=recent)
+
+        time_order = request.GET.get('timeorder', '')
+        if time_order:
+            if time_order == 'time_down':
+                recent_projects = recent_projects.order_by('-float_date')
+            elif time_order == 'time_up':
+                recent_projects = recent_projects.order_by('float_date')
         # sort = request.GET.get('sort', "")
         # if sort:
         #     if sort == "students":
@@ -76,16 +94,19 @@ class ProjectView(View):
         #
         # org_nums = all_orgs.count()  # 课程总数
 
+        proj_nums = recent_projects.count()
         # 对课程机构进行分页
         try:
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
             page = 1
-        p = Paginator(all_projects, 5, request=request)
-        all_projects = p.page(page)
+        p = Paginator(recent_projects, 5, request=request)
+        recent_projects = p.page(page)
 
         return render(request, 'project-list.html', {
-            'all_projects': all_projects,
+            'all_projects': recent_projects,
             'category': category,
+            'time_order': time_order,
+            'proj_nums': proj_nums,
+            'time_recent':time_recent,
         })
-
